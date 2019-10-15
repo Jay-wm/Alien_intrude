c3c4bfeadcb21a425114428827f26e7034fab54b
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-10-15 21:00:33 +0800

    1.14
    1.14

--- alien.py
+++ alien.py
@@ -8,7 +8,6 @@ class Alien(Sprite):
     def __init__(self, ai_settings, screen):
         """初始化外星人并设置其起始坐标"""
         super(Alien, self).__init__()
-
         self.screen = screen
         self.ai_settings = ai_settings
 
@@ -22,8 +21,25 @@ class Alien(Sprite):
 
         # 存储外星人的准确位置
         self.x = float(self.rect.x)
-
-
+        
+    def check_edges(self):
+        """检查外星人是否到达边缘"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+        
+        
+    def update(self):
+        """向右移动外星人"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.aliens_speed_direction)
+        self.rect.x = self.x
+        
+        
     def blitme(self):
         """在指定位置绘制外星人"""
         self.screen.blit(self.image, self.rect)
+        
+        
+    
--- settings.py
+++ settings.py
@@ -3,17 +3,24 @@ class Settings():
     def __init__(self):
         """初始化游戏设置"""
         # 屏幕设置
-        self.screen_width=1140
+        self.screen_width=1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
-        # 飞船速度因子
+        # 飞船设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_hight = 15
-        self.bullet_color =60, 60, 60
-        self.bullets_allowed = 3
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 20
+        
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.aliens_drop_speed = 2
+        # 1表示向右移动，-1表示向左移动
+        self.aliens_speed_direction = 1
         
