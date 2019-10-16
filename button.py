da2b1cdc99ca0314734c66600941f3922e73dc39
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-10-16 20:17:39 +0800

    Update button.py

--- button.py
+++ button.py
@@ -25,7 +25,7 @@ class Button():
         """将msg渲染为图像，并使其在按钮上居中"""
         self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect_center = self.rect.center
+        self.msg_image_rect.center = self.rect.center
         
         
     def draw_button(self):
